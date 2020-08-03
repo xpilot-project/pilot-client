f93f8c3d3c5490f53a31dc1f248606aae7b1b8d4
Justin Shannon <zla.jshannon@gmail.com>
2020-08-02 19:55:35 -0700

    Update version

--- XplaneAdapter/XplaneConnectionManager.cs
+++ XplaneAdapter/XplaneConnectionManager.cs
@@ -38,7 +38,7 @@ namespace XPilot.PilotClient.XplaneAdapter
 {
     public class XplaneConnectionManager : EventBus, IXplaneConnectionManager
     {
-        const int MIN_PLUGIN_VERSION = 1321; // 1.3.10 = 1310
+        const int MIN_PLUGIN_VERSION = 1322; // 1.3.10 = 1310
 
         [EventPublication(EventTopics.NotificationPosted)]
         public event EventHandler<NotificationPostedEventArgs> NotificationPosted;
