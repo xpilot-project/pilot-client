aab77e337a29317ef9bfa0f7720ac7e6b825b626
Justin Shannon <zla.jshannon@gmail.com>
2020-08-20 21:11:32 -0700

    Increment version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.30")]
-[assembly: AssemblyInformationalVersion("1.3.30")]
+[assembly: AssemblyVersion("1.3.31")]
+[assembly: AssemblyInformationalVersion("1.3.31")]
