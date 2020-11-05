6d8da0947cfd743760578bb49facd49f3ba56228
Justin Shannon <zla.jshannon@gmail.com>
2020-11-04 21:39:58 -0800

    Up version number for instancing validation build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.32")]
-[assembly: AssemblyInformationalVersion("1.3.32")]
+[assembly: AssemblyVersion("1.3.33")]
+[assembly: AssemblyInformationalVersion("1.3.33")]
